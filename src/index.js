3e075fd648749ee2362548861f1db54dd48b5081
Francisco Echeverria <francfox540@gmail.com>
2024-05-25 13:55:22 -0600

    primera carga

---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { JqueryExtend } from './limbo-react-jquery-extend';
---
+++ src/limbo-react-jquery-extend.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function JqueryExtend(fnName,Component){
+	
+	const __TARGETS=[];
+	const mode = process.env.NODE_ENV;
+ 
+
+	const p = new Promise((resolve,reject)=>{		
+		const hotRender=function(NewComponent){
+			//console.log('hotRender');
+			__TARGETS.map((x)=>{
+	
+				ReactDOM.render(
+				  <NewComponent {...x.params}/>,
+				  x.t
+				);
+			  })
+		}
+
+ 
+		if(typeof jQuery !== 'undefined'){
+
+			jQuery.fn[fnName] = function (params) {
+				
+			
+				this.each(function () {
+					__TARGETS.push({t:this,params:params});
+				
+					ReactDOM.render(
+						<Component {...params}/>,
+						this
+					);
+				});
+				if(mode == 'development'){			
+					resolve(hotRender);
+				}
+				
+			};
+			
+		}
+		 
+	})
+	return p;
+	
+}
